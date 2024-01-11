512f752da73051eb9e4b497ec56f83720bbca20b
Praveen Kishore Pragya <72307064+praveen-kishore-pragya@users.noreply.github.com>
2024-01-11 17:13:08 +0100

    Add files via upload

--- Sundown Studio Clone/script.js
+++ Sundown Studio Clone/script.js
@@ -1,4 +1,26 @@
-let elem1 = document.querySelector("#elem1");
-elem1.style.after = {
-    
-}
+let showcase = document.querySelector(".showcase");
+let fixedImage = document.querySelector(".fixedImage");
+
+showcase.addEventListener("mouseenter",function(){
+    fixedImage.style.display = "block";
+    // display property set to block starts on a new line and takes up the available screen width.
+});
+
+showcase.addEventListener("mouseleave",function(){
+    fixedImage.style.display = "none";
+});
+
+let elem = document.querySelectorAll(".elem");
+// var a = elem1.getAttribute("data-image")
+console.log(elem);
+elem.forEach(function(element){
+    {
+        element.addEventListener("mouseenter",function(){
+            let image = element.getAttribute("data-image");
+            // fixedImage.style.backgroundColor = "blue"
+            console.log(image);
+            console.log(`url(${"image"})`);
+            fixedImage.style.backgroundImage = `url(${"image"})`;
+        });
+    }
+});
