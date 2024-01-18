f40b07ee4a54dc5dda1ecdd49b3cf95b663210ec
Praveen Kishore Pragya <72307064+praveen-kishore-pragya@users.noreply.github.com>
2024-01-18 11:35:02 +0100

    Add files via upload

--- Retro Design/script.js
+++ Retro Design/script.js
@@ -2,3 +2,27 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+// gsap.to(".main",{
+//     y: 900,
+//     // rotate:360,
+//     duration: 0,
+//     scale: 0.4,
+//     // delay: 1
+// })
+
+// gsap.to(".main",{
+//     y: -550,
+//     // rotate:360,
+//     scale:0.4,
+//     duration: 0.7,
+//     delay: 1
+// })
+
+// gsap.to(".main",{
+//     y:0,
+//     rotate:-360,
+//     scale:1,
+//     duration: 1,
+//     delay: 3
+// })
